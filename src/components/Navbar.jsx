c7c9dcc74a062bf063249bcb0fc4bcf327e739c4
bhar1gitr <97161064+bhar1gitr@users.noreply.github.com>
2022-01-12 19:01:16 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export class Navbar extends Component {
             <>
    <nav id="nav" className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand text-danger"><b>Bharat Sharma</b></a>
+    <div className="navbar-brand text-danger"><b>Bharat Sharma</b></div>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
